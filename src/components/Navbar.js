d194c2fcefbda93ecd0c2edd0c76e98995c61645
Abbas Ali <Abbas152052@gmail.com>
2022-10-17 00:57:08 +0500

    #

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,8 +16,8 @@ export default function Navbar(props) {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item"><a className="nav-link active" aria-current="page" href="/">Home</a></li>
-            <li className="nav-item"><a className="nav-link" href="/">{props.About}</a></li>
+            <li className="nav-item"><a className="nav-link active" aria-current="page" href="#">Home</a></li>
+            {/* <li className="nav-item"><a className="nav-link" href="/">{props.About}</a></li> */}
             {/* For Routers  */}
             {/* <li className="nav-item"><Link className="nav-link active" aria-current="page" to="/">Home</Link></li> */}
             {/* <li className="nav-item"><Link className="nav-link" to="/about">{props.About}</Link></li> */}
